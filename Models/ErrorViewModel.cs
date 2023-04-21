ee877ceeefc949dca0fdff01327f1aec9d473165
Training <training@ip-172-31-17-26.us-east-2.compute.internal>
2023-04-21 20:07:09 +0000

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace azuregithubwebapp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
